b809661b282ee3a4452a15f0b8b2aaef00d3ecca
Joy Kim <wlasl0121@gmail.com>
2022-08-10 00:05:56 +0900

    CRA styles setting

--- front_web/src/App.tsx
+++ front_web/src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import { BrowserRouter as Router } from "react-router-dom";
+import ScrollToTop from "./Libs/ScrollToTop";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+            <ScrollToTop />
+        </Router>
+    );
 }
-
-export default App;
